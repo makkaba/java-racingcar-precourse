7d38928c64837a4ba7c57d649051b3b3d950ed97
Jeff Chung <drifterz303@gmail.com>
2021-05-01 19:06:46 +0900

    feat(ValidationUtil): 자동차 이름 길이 유효성 검사를 추가한다

--- src/main/java/utils/ValidationUtil.java
+++ src/main/java/utils/ValidationUtil.java
@@ -1,4 +1,13 @@
 package utils;
 
 public class ValidationUtil {
+	public static final int MAX_NAME_LENGTH = 5;
+	public static final int MIN_NAME_LENGTH = 0;
+
+	public static boolean ValidNameLength(String name) {
+		if(name.length() > MIN_NAME_LENGTH && name.length() <= MAX_NAME_LENGTH){
+			return true;
+		}
+		return false;
+	}
 }
--- src/test/java/racingcar/ValidationUtilTest.java
+++ src/test/java/racingcar/ValidationUtilTest.java
@@ -1,4 +1,24 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utils.ValidationUtil;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ValidationUtilTest {
+
+	@Test
+	@DisplayName("자동차의_이름은_5자_초과이면_false")
+	void 자동차의_이름은_5자_초과이면_false(){
+		boolean actual = ValidationUtil.ValidNameLength("jeffchung");
+		assertThat(actual).isFalse();
+	}
+
+	@Test
+	@DisplayName("자동차의_이름은_5자_이하이면_true")
+	void 자동차의_이름은_5자_이하이면_true(){
+		boolean actual = ValidationUtil.ValidNameLength("jeffc");
+		assertThat(actual).isTrue();
+	}
 }
